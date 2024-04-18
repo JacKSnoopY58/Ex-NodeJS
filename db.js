4ab79ee4a7398f9a9cf6132419d49c1d292f8237
JacKSnoopY58 <titijack58@gmail.com>
2024-04-18 23:11:51 +0700

    chage DB

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
-    await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/EX-JS`);
+    await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/e-commerce`);
       
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
