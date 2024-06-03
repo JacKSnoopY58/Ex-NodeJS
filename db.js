ecf7d29f2219e95481f861ccd83abaa0ae7d047f
Ittikorn Chaikittikul <titijack58@gmail.com>
2024-06-03 23:24:26 +0700

    Update db.js

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
-    await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/e-commerce`);
-      
+    await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/EX-JS`);
+    console.log("Connect Database");
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
